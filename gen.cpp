7ae6b6f5c2114cb377ca118ade1429766f9fe0dd
EG2107 <105222195+EG2107@users.noreply.github.com>
2024-06-24 23:01:09 +0300

    ...

---
+++ gen.cpp
@@ -0,0 +1,73 @@
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <random>
+#include <unordered_map>
+#include <vector>
+const int MSG_AMOUNT = 1e4;
+
+std::mt19937 rng(42239777);
+std::string alphabet;
+std::vector<std::string> phone_numbers, surnames, first_names, last_names;
+
+std::string gen_message(){
+    std::uniform_int_distribution<int> dis(32, 126);
+    std::string res = "";
+    int len = rng() % 100 + 1;
+    for (int i = 0; i < len; ++i){
+        res += char(dis(rng));
+    }
+    return res;
+}
+
+std::string gen_ip(){
+    std::string res;
+    for (int i = 0; i < 4; ++i){
+        int block = rng() % 256;
+        res += std::to_string(block);
+        if (i < 3){
+            res += '.';
+        }
+    }
+    return res;
+}
+
+std::string gen_user_data(){
+    return surnames[rng() % surnames.size()] + ' ' 
+        + first_names[rng() % first_names.size()] + ' ' 
+        + last_names[rng() % last_names.size()] + ' ' 
+        + phone_numbers[rng() % phone_numbers.size()] + ' ' 
+        + gen_ip();
+}
+
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+    for (char c = 'a'; c <= 'z'; ++c){
+        alphabet += c;
+    }
+    for (char c = 'A'; c <= 'Z'; ++c){
+        alphabet += c;
+    }
+    for (char c = '0'; c <= '9'; ++c){
+        alphabet += c;
+    }
+    alphabet += ' ';
+
+    std::fstream fout("in.log");
+
+    phone_numbers = {"+7911-123-45-67", "+7921-239-42-69", "567-89-10", "8(931)523-65-67", "+79541234567", "8921-123-45-69"};
+    first_names = {"Иван", "Василий", "Константин", "Петр", "Алексей", "Дмитрий"};
+    surnames = {"Иванов", "Петров", "Сидоров", "Алексеев", "Авербах"};
+    last_names = {"Иванович", "Петрович", "Константинович", "Евгеньевич"};
+
+    for (int i = 0; i < MSG_AMOUNT; ++i){
+        fout << "'" << gen_message() << "' was sent from " << gen_user_data() << " to " << gen_user_data() << '\n';
+    }
+
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,196 @@
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+std::vector<std::string> phone_number_templates;
+
+void input_templates(){
+    std::ifstream fin_templates("phone_number_templates.txt");
+    std::string phone_template;
+    while (std::getline(fin_templates, phone_template)){
+        phone_number_templates.push_back(phone_template);
+    }
+}
+
+bool is_digit(char c){
+    return '0' <= c && c <= '9';
+}
+
+unsigned int ip_to_ul(std::string ip){
+    ip += '.';
+    unsigned int ans = 0, block = 0;
+    for (char c : ip){
+        if (c == '.'){
+            ans *= 256;
+            ans += block;
+            block = 0;
+        } else {
+            block *= 10;
+            block += c - '0';
+        }
+    }
+    return ans;
+}
+
+std::string ul_to_ip(unsigned int ul_ip){
+    std::vector<std::string> ip;
+    for (int i = 0; i < 4; ++i){
+        ip.push_back(std::to_string(ul_ip % 256));
+        ul_ip /= 256;
+    }
+    std::string res;
+    for (int i = ip.size() - 1; i >= 0; --i){
+        res += ip[i];
+        if (i > 0){
+            res += '.';
+        }
+    }
+    return res;
+}
+
+long long phone_to_ll(std::string phone){
+    long long res = 0;
+    for (char c : phone){
+        if (is_digit(c)){
+            res *= 10;
+            res += c - '0';
+        }
+    }
+    return res;
+}
+
+bool is_phone_number(std::string str){
+    for (std::string phone_template : phone_number_templates){
+        if (str.size() != phone_template.size()){
+            continue;
+        }
+        bool flag = true;
+        for (int i = 0; i < str.size(); ++i){
+            if (str[i] != phone_template[i]){
+                if (is_digit(str[i])){
+                    if ((i == 0 && phone_template[0] == '8' && phone_template.size() > 9) || (i == 1 && phone_template[0] == '+')){
+                        flag = false;
+                        break;
+                    }
+                } else {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        if (flag){
+            return true;
+        }
+    }
+    return false;
+}
+
+bool is_ip(std::string str){
+    int cnt_dots = 0, cnt_digits_in_block = 0;
+    for (char c : str){
+        if (c == '.'){
+            cnt_dots++;
+            cnt_digits_in_block = 0;
+        } else if (is_digit(c)){
+            cnt_digits_in_block++;
+            if (cnt_digits_in_block > 3){
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    return cnt_dots == 3;
+}
+
+bool is_russian(std::string str){
+    std::string rus = "ая";
+    for (int i = 0; i < str.size(); ++i){
+        if (!(rus[0] <= str[i] && str[i] <= rus[1])){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    setlocale(LC_ALL, "Russian");
+
+    input_templates();
+
+    std::ifstream fin_logs("in.log");
+    std::ofstream fout_logs("out.log");
+    std::ofstream fout_map("convert_map.txt");
+
+    std::unordered_map<long long, unsigned int> mp_phones;
+    std::unordered_map<unsigned int, unsigned int> mp_ips;
+    std::unordered_map<std::string, unsigned int> mp_names;
+
+    std::string line;
+    while (std::getline(fin_logs, line)){
+        std::stringstream sstream(line);
+        std::string word;
+        while (sstream >> word){
+            if (is_phone_number(word)){
+                long long phone = phone_to_ll(word);
+                if (!mp_phones.count(phone)){
+                    mp_phones[phone] = mp_phones.size();
+                }
+                fout_logs << "phone" << mp_phones[phone] << " ";
+            } else if (is_ip(word)){
+                unsigned int ip = ip_to_ul(word);
+                if (!mp_ips.count(ip)){
+                    mp_ips[ip] = mp_ips.size();
+                }
+                fout_logs << "ip" << mp_ips[ip] << " ";
+            } else if (is_russian(word)){
+                if (!mp_names.count(word)){
+                    mp_names[word] = mp_ips.size();
+                }
+                fout_logs << "name" << mp_names[word] << " ";
+            } else {
+                fout_logs << word << " ";
+            }
+        }
+        fout_logs << "\n";
+    }
+
+    std::vector<std::pair<unsigned int, long long>> phone_numbers_to_sort;
+    for (auto i : mp_phones){
+        phone_numbers_to_sort.push_back(std::make_pair(i.second, i.first));
+    }
+    std::sort(phone_numbers_to_sort.begin(), phone_numbers_to_sort.end());
+    fout_map << "Phone numbers:\n";
+    for (auto i : phone_numbers_to_sort){
+        fout_map << i.second << " " << i.first << "\n";
+    }
+    fout_map << '\n';
+
+    std::vector<std::pair<unsigned int, unsigned int>> ip_map_to_sort;
+    for (auto i : mp_ips){
+        ip_map_to_sort.push_back(std::make_pair(i.second, i.first));
+    }
+    std::sort(ip_map_to_sort.begin(), ip_map_to_sort.end());
+    fout_map << "IPs:\n";
+    for (auto i : ip_map_to_sort){
+        fout_map << ul_to_ip(i.second) << " " << i.first << "\n";
+    }
+    fout_map << '\n';
+
+    fout_map << "Names:\n";
+    for (auto i : mp_names){
+        fout_map << i.first << " " << i.second << "\n";
+    }
+    fout_map << '\n';
+
+
+    return 0;
+}
